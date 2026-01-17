8e21b7871f43702c65de14d9f7ed496aa25de5bf
Joseph <joseph.gb369@gmail.com>
2026-01-17 18:29:57 +0530

    initial commit

---
+++ models.py
@@ -0,0 +1,51 @@
+from app import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200))
+    semester = db.Column(db.String(20))
+    branch = db.Column(db.String(50))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject_name = db.Column(db.String(100))
+    semester = db.Column(db.String(20))
+
+class StudentSubject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'))
+
+class Assessment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'))
+    score = db.Column(db.Integer)
+    level = db.Column(db.String(20))
+    taken_on = db.Column(db.DateTime, default=datetime.utcnow)
+
+class StudyPreference(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    daily_study_time = db.Column(db.Integer)
+
+class StudyPlan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'))
+    allocated_time = db.Column(db.Integer)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Progress(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'))
+    completion_percentage = db.Column(db.Integer)
+    last_updated = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<User {self.name}>"
